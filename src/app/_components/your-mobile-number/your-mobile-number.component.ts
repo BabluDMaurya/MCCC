04b108ddb9c8c1030bfffef9db6a73153d9c487c
BabluDMaurya <bablu.wdipl@gmail.com>
2021-12-13 15:42:24 +0530

    mobileCodeAdded

--- src/app/_components/your-mobile-number/your-mobile-number.component.ts
+++ src/app/_components/your-mobile-number/your-mobile-number.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import countries from '../../_files/countries.json';
 
 @Component({
   selector: 'app-your-mobile-number',
@@ -7,9 +8,11 @@ import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/fo
   styleUrls: ['./your-mobile-number.component.scss']
 })
 export class YourMobileNumberComponent implements OnInit {
+  public codeList:{id:number, name:string,mobileCountryCode:string}[] = countries;
+  // public countryList:{id:number, name:string, code:string}[] = countries;
   @Input() parent: FormGroup | any;
   @Input() submit: any;
-  codeList: any = ['+91', '+101', '+21'];
+  // codeList: any = ['+91', '+101', '+21'];
   selectedCode = '+91';
   constructor() { }
   ngOnInit(): void {        
